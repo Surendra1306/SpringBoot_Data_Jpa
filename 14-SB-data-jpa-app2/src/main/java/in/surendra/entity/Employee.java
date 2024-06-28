c06a328ada105cfb49e421b851ffc2974c1642c5
Surendra <118037065+Surendra1306@users.noreply.github.com>
2024-06-28 11:54:28 +0530

    Update Employee.java

--- 14-SB-data-jpa-app2/src/main/java/in/surendra/entity/Employee.java
+++ 14-SB-data-jpa-app2/src/main/java/in/surendra/entity/Employee.java
@@ -28,43 +28,25 @@ public class Employee {
 	public Integer getEid() {
 		return eid;
 	}
-
-
-
 	public void setEid(Integer eid) {
 		this.eid = eid;
 	}
-
-
-
 	public String getEname() {
 		return ename;
 	}
-
-
-
 	public void setEname(String ename) {
 		this.ename = ename;
 	}
-
-
-
 	public Integer getEsalary() {
 		return esalary;
 	}
-
-
-
 	public void setEsalary(Integer esalary) {
 		this.esalary = esalary;
 	}
-
-
-
+	
 	@Override
 	public String toString() {
 		return "Employee [eid=" + eid + ", ename=" + ename + ", esalary=" + esalary + "]";
 	}
 	
-
 }
